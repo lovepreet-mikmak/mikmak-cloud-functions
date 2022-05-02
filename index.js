0700bb234d00d7a8443d50a3d360e12a3e57a8fc
Lovepreet Singh <lovesingh@bebotechnologies.com>
2022-05-02 15:33:07 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ app.get("/",(req, res)=>{
     const msg = req.query.message || "hello world";
         res.status(200).json({message: msg});
     })
+app.get("/new",(req, res)=>{
+    const msg = req.query.message || "new msg";
+        res.status(200).json({message: msg});
+    })
 exports.app =  app;
