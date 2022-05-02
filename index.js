9b0eac30cf23864d1732b1af923dcd5236cb8bdd
Lovepreet Singh <lovesingh@bebotechnologies.com>
2022-05-02 19:59:13 +0530

    modified index.js

--- index.js
+++ index.js
@@ -4,11 +4,13 @@
  * @param {!Object} event Event payload.
  * @param {!Object} context Metadata for the event.
  */
+var a = 25;
  exports.helloPubSub = (event, context) => {
     const message = event.data
       ? Buffer.from(event.data, 'base64').toString()
       : 'Hello, World';
       console.log("event---", event, "timestamp---", context.timestamp);
     console.log("message is---", message);
+    console.log("A is--", a);
   };
   
