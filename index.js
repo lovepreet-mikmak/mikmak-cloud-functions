a38a794f93ccf07ed58e932ba3a868e043947553
Lovepreet Singh <lovesingh@bebotechnologies.com>
2022-05-06 13:42:50 +0530

    integrated google cloud storage

--- index.js
+++ index.js
@@ -1,34 +1,82 @@
-/**
- * Triggered from a message on a Cloud Pub/Sub topic.
- *
- * @param {!Object} event Event payload.
- * @param {!Object} context Metadata for the event.
- */
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
-exports.helloPubSub = async (event, context) => {
-    initializeApp();
+const { Storage } = require('@google-cloud/storage');
+const storage = new Storage();
+const checkBucketExistence = async (name = "") => {
+    try {
+        const [buckets] = await storage.getBuckets();
 
-    const db = getFirestore();
+        console.log('Buckets:', buckets);
+        return buckets.findIndex(bucket => bucket.name === name) >= 0 ? true : false;
+    }
+    catch (error) {
+        console.log("error occured during fetching buckets list:-", error);
+        return false;
+    }
+};
+const createBucket = async (name = "", fileName = "", content = "") => {
+    // Creates the new bucket
+    try {
+        await storage.createBucket(name);
+        console.log(`Bucket ${name} created.`);
+        return true;
+    }
+    catch (error) {
+        console.log("error occured during creating bucket:-", error);
+        return false;
+    }
+};
+const checkFileExistence = async (bucketName = "", fileName = "") => {
+    const [files] = await storage.bucket(bucketName).getFiles();
+
+    console.log("Files:", files);
+    return files.findIndex(file => file.name === fileName) >= 0 ? true : false;;
+}
+const createFile = async (bucketName = "", fileName = "", content = "") => {
+    try {
+        await storage.bucket(bucketName).file(fileName).save(content);
+
+        console.log(
+            `${fileName} with content ${content} uploaded to ${bucketName}.`
+        );
+        return true;
+
+    } catch (error) {
+        console.log("error while creating file:-", error);
+        return false;
+    }
+};
+const updateFile = async (bucketName = "", fileName = "", content = "") => {
+    try {
+        await storage.bucket(bucketName).file(fileName).delete();
+        await storage.bucket(bucketName).file(fileName).save(content);
+        console.log(`${fileName} updated`);
+
+    } catch (error) {
+        console.log("error while updating file:-", error);
+        return false;
+    }
+}
+
+exports.helloPubSub = (event, context) => {
+    const bucketName = "bucket1";
+    const fileName = "logs.txt";
+    const content = `lastTimestamp=${context.timestamp}`
     const message = event.data
         ? Buffer.from(event.data, 'base64').toString()
         : 'Hello, World';
     console.log("message is---", message);
-    // const docRef = db.collection('logs').doc('timeStamp');
 
-    // await docRef.set({
-    //     timeStamp: "hello"
-    // });
-    const snapshot = await db.collection('logs').get();
-    snapshot.forEach((doc) => {
-        console.log(doc.id, '=>', doc.data());
-    });
-    // if (lastTimestamp) {
-
-    //     console.log("The difference is ---", new Date(context.timestamp) - new Date(lastTimestamp));
-    // } else {
-    //     console.log("This is first Time execution call");
-    // }
-
-
+    const isBucket = checkBucketExistence(bucketName);
+    if (!isBucket) {
+        const bucketAdded = createBucket(bucketName, fileName, content);
+        if (bucketAdded) {
+            createFile(bucketName, fileName, content);
+        }
+    } else {
+        const isFile = checkFileExistence(bucketName, fileName);
+        if (!isFile) {
+            createFile(bucketName, fileName, content);
+        } else {
+            updateFile(bucketName, fileName, content);
+        }
+    }
 };
